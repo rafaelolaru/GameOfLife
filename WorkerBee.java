c7ab524505daf7faa57410a97b276b57d72abc13
Rares Sabau <raressabau1284@gmail.com>
2023-11-07 21:35:05 +0200

    Created specific class for each type of bee

---
+++ WorkerBee.java
@@ -0,0 +1,16 @@
+// WorkerBee class with increased lifespan
+class WorkerBee extends Bee {
+    private static final double CHANCE_TO_GET_FOOD = 0.75;
+
+    public WorkerBee(HiveEnvironment environment, BeeLifecycleListener listener) {
+        super(100, environment, listener, "WorkerBee"); // increased lifespan to 100
+    }
+
+    @Override
+    public void performDailyTask() {
+        if (Math.random() < CHANCE_TO_GET_FOOD) {
+            environment.addFood();
+            System.out.println("Worker bee collected food.");
+        }
+    }
+}
